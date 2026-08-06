76884dfb89548c536e2231bab365fd0756df7563
Areeba Khalid <areebakhalid877@gmail.com>
2026-08-05 22:21:01 -0700

    Update profile and hero changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { Typewriter } from "react-simple-typewriter";
-import profile from "../assets/profile.png";
+import profile from "../assets/Profile.png";
 import { FaGithub, FaLinkedinIn, FaFacebookF, FaDownload } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 
